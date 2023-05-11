1851448cb049d5d4a2f5abec1ceef7848819c7a4
Jialu <51308899+jbddqdddqdgwdddd@users.noreply.github.com>
2023-05-11 22:17:18 +1200

    Front-end restructure

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import React, { useState,useEffect, useMemo } from "react";
 import { BrowserRouter as Router, Route ,Routes} from "react-router-dom";
+import Login from './Login';
+import Register from './Register';
+import Search from './Search';
 
 
 function App() {
@@ -10,6 +13,8 @@ function App() {
       <Router>
             <Routes>
                 <Route exact path="/" element={<Login/>} />
+                <Route exact path="/register" element={<Register/>} />
+                <Route exact path="/search" element={<Search/>} />
             </Routes> 
           </Router>
     </div>
